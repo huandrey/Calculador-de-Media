5228ed6e914cf6732a174c0dd1cb226c7b545281
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2021-01-27 01:46:46 -0300

    corrigindo importacao

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import { React } from 'react';
 
-import './index.module.css'
 
 import Container from '../components/Container';
 // Compoente sem estado, ou seja, uma funcao normal que retorna jsx
