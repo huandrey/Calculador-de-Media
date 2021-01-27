e757df6e52988d5d51eb2f298d239f28fc66c79d
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2021-01-27 01:44:57 -0300

    adicionando rota/arquivo principal

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { React } from 'react';
+
+import './index.module.css'
+
+import Container from '../components/Container';
+// Compoente sem estado, ou seja, uma funcao normal que retorna jsx
+
+export default function App({ Components, pageProps }) {
+    return <Container />;
+}
+
+
+
